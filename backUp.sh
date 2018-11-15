71489b4256c6748a9e3b0cb9103abf242dbe1842
Tom <35616860+Caedan@users.noreply.github.com>
2018-11-15 21:38:29 +0000

    Create backUp.sh

---
+++ backUp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "1. Backup"
+echo "2. Restore"
+echo
+echo "Select a menu option please:"
+read option
+if [ $option = 1 ]
+then
+        echo "Specify the directory or file you wish to backup:"
+        read dirfile
+        while [ ! -r "$dirfile" ]
+        do
+                        echo "Type in a valid directory or file path:"
+                        read dirfile
+        done
+        echo "Currently mounted storage devices:"
+        echo
+        ls /mnt
+        echo "Specify the location where you want to save your backup: "
+        read location
+
+fi
