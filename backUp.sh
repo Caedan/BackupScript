c7b50cd6ee14158e598ada1e749685e8fa2c7f7c
Tom <35616860+Caedan@users.noreply.github.com>
2018-11-24 23:50:22 +0000

    Backup on mounted devices possible
    Added a few error handling statements
    Another one for the location variable and the recovery variable needs to be added !

--- backUp.sh
+++ backUp.sh
@@ -3,12 +3,17 @@
 echo "1. Backup"
 echo "2. Restore"
 echo
-echo "Select a menu option please:"
+echo "Select a menu option please: "
+
+# Saves user input in $option variable
 read option
+
 if [ $option = 1 ]
 then
         echo "Specify the directory or file you wish to backup:"
         read dirfile
+
+        # This while loop checks if the user typed in an existing directory or file
         while [ ! -r "$dirfile" ]
         do
                         echo "Type in a valid directory or file path:"
@@ -16,13 +21,21 @@ then
         done
         echo "Currently mounted storage devices:"
         echo
-	#Need to figure out a way to display storage devices, internal and 
-	#mounted.
-
+        ls /mnt
         echo "Specify the location where you want to save your backup: "
         read location
-	test_file="test.tgz"
-	tar czf $location/$test_file $dirfile
 
+        # Tar creates an archive with the specified name
+        # compresses it and saves it in the specified location
+        sudo tar -cpzf /mnt/$location/backup.tar.gz --absolute-names $dirfile
 
+fi
+elif [ $option = 2 ]
+then
+        echo "Specify the backup you wish to deploy:"
+        read recoveryTar
+fi
+else
+then
+        echo "You can only type in a number displayed on the menu"
 fi
